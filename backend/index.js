5d06a314831762ca0b567b64f08d5e778eb7364f
Apoorv Sharma <80025553+ars-2107@users.noreply.github.com>
2024-03-15 23:31:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,20 +7,7 @@ import "dotenv/config";
 import routes from "./src/routes/index.js";
 
 const app = express();
-
-const allowedOrigins = ["https://raveo.vercel.app"];
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = "The CORS policy for this site does not allow access from the specified origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -40,3 +27,4 @@ mongoose.connect(process.env.MONGODB_URL).then(() => {
   console.log({ err });
   process.exit(1);
 });
+
